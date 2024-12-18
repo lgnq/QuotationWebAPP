c04c1d42af0d90f9504ed71bce5a0d133ad9e268
lgnq <dzzxzz@gmail.com>
2024-12-18 11:59:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ const number = document.querySelectorAll(".numbers").forEach(function (item) {
       inputValue.innerText += e.target.innerHTML.trim();
     });
   });
+
+  const calculate = document
+  .querySelectorAll(".operations")
+  .forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      console.log(e.target.innerHTML);
+    });
+  });  
+
+  let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
+
+  if (!isNaN(lastValue) && e.target.innerHTML === "=") {
+      inputValue.innerText = eval(inputValue.innerText);
+  } else if (e.target.innerHTML === "AC") {
+      inputValue.innerText = 0;
+  } else if (e.target.innerHTML === "DEL") {
+      inputValue.innerText = inputValue.innerText.substring(0,inputValue.innerText.length - 1);
+      if (inputValue.innerText.length == 0) {
+          inputValue.innerText = 0;
+      }
+  } else {
+      if (!isNaN(lastValue)) {
+          inputValue.innerText += e.target.innerHTML;
+      }
+  }  
