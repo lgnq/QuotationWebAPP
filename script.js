1e4f9f6cf6883548107c2bf728ecc1ec2d40dc79
lgnq <dzzxzz@gmail.com>
2024-12-18 11:55:44 +0800

    create a calculater

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Variable Identifier
-const exampleDeclaration = document.getElementById("placeIdHere");
+const inputValue = document.getElementById("user-input");
 
 // Function Declaration
-function ExampleFunctionName(params) {}
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      if (inputValue.innerText === "NaN") {
+        inputValue.innerText = "";
+      }
+      if (inputValue.innerText === "0") {
+        inputValue.innerText = "";
+      }
+      inputValue.innerText += e.target.innerHTML.trim();
+    });
+  });
